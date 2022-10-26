7bb29fa371de882e30375234259e5255e4539f40
L3x <hleon0507@gmail.com>
2022-10-25 20:27:01 -0500

    add image to grid

---
+++ src/MovieCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function MovieCard({ movie }) {
+  const imageUrl = "http://image.tmdb.org/t/p/w300" + movie.poster_path;
+  return (
+    <li>
+    <img src={imageUrl} alt={movie.title} />
+    {movie.title}
+  </li>
+  );
+}
--- src/MoviesGrid.jsx
+++ src/MoviesGrid.jsx
@@ -1,15 +1,14 @@
 import React from "react";
+import { MovieCard } from "./MovieCard";
 import movies from "./movies.json";
 
 export function MoviesGrid() {
   //    console.log(movies);// see all content in console
   return (
     <ul>
-      {movies.map(
-        (movie) => (
-          <li key={movie.id}>{movie.title}</li>
-        )
-      )}
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
     </ul>
   );
 }
