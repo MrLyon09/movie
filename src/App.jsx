1b943a37857bf92f2720758f707d5d55fe1c95de
L3x <hleon0507@gmail.com>
2022-10-25 19:24:53 -0500

    Create Web Page Movies in react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+export function App() {
+  return (
+    <div>
+      <header>
+        <h1>Movies</h1>
+      </header>
+      <main>
+        <ul>
+            <li>Movie1</li>
+            <li>Movie2</li>
+            <li>Movie3</li>
+        </ul>
+
+      </main>
+    </div>
+  );
+}
