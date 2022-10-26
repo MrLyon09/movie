ce9dcdd555869f9bda164c628adbd5f1531b7b0d
L3x <hleon0507@gmail.com>
2022-10-25 21:20:36 -0500

    create style for title movies in app

--- src/MoviesGrid.jsx
+++ src/MoviesGrid.jsx
@@ -1,11 +1,11 @@
-import React from "react";
 import { MovieCard } from "./MovieCard";
 import movies from "./movies.json";
+import styles from './MoviesGrid.module.css'
 
 export function MoviesGrid() {
   //    console.log(movies);// see all content in console
   return (
-    <ul>
+    <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
