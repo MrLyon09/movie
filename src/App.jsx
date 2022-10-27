f89b538c47545fe375a8224c3e891d8b39c0c0c9
L3x <hleon0507@gmail.com>
2022-10-27 09:01:15 -0500

    Rutas a home y movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import { MoviesGrid } from "./components/MoviesGrid";
-import styles from './App.module.css'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import styles from "./App.module.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { MovieDetails } from "./pages/MovieDetails";
+import { LandingPage } from "./pages/LandingPage";
 
 export function App() {
   return (
     <Router>
       <header>
-        {/* <h1 className={styles.title}>Movies FREE</h1> */}
+        <Link to="/">
+          <h1 className={styles.title}>Movies FREE</h1>
+        </Link>
+
         <Link to="/">Home</Link>
         <br />
         <Link to="/movie">movie</Link>
       </header>
       <main>
         <Switch>
-          <Route exact path="/movie">Movie</Route>
-          <Route exact path="/">Home</Route>
-          <Route  path="/">ERROR 404 - PAGE NO FOUND</Route>
+          <Route exact path="/movie">
+            <MovieDetails />
+          </Route>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/">ERROR 404 - PAGE NO FOUND</Route>
         </Switch>
       </main>
     </Router>
