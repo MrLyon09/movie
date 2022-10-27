29af6e43c7d7b437256985d84c0f5ab18f638128
L3x <hleon0507@gmail.com>
2022-10-27 17:06:52 -0500

    usando useState

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,19 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { MovieCard } from "./MovieCard";
 import movies from "./movies.json";
 import styles from "./MoviesGrid.module.css";
 
 export function MoviesGrid() {
-  //    console.log(movies);// see all content in console
-  useEffect(() => [
+  const [movies, setMovies] = useState([]);
+  
+  useEffect(() => {
     fetch("https://api.themoviedb.org/3/discover/movie", {
       headers: {
         Authorization:
           "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGRiYzFlMzJhYTFkMWQ0Y2EyNTkzOTkyYjQ0ODI4NyIsInN1YiI6IjYzMTBkODM0MGQyOTQ0MDA4MWQ4YTA1YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HuS3SA1JbgIbQ1LaNg2nn29eCoiy8DVT6gUK5rNNvLM",
         "Content-type": "application/json;charset=utf-8",
       },
-    }),
-  ]);
+    }).then(result =>result.json()).then((data)=>{
+      movies = data.results;
+    });
+  });
+
+
   return (
     <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
